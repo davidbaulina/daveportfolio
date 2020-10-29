54a1319bdae2b3b09f9b7f797a8773785fc9d0ab
escalofraimochilanga <davidbaulina@gmail.com>
2020-10-29 16:55:39 -0300

    agrega router. Agrega componente Gallery

---
+++ src/components/Gallery.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="gallery">
+    <div class="gallery-panel"
+         v-for="image in images"
+         :key="image.id">
+      <img :src="thumbUrl(image.filename)">
+    </div>
+  </div>
+</template>
+
+<script>
+import images from '@/photos.json';
+export default {
+  name: 'Gallery',
+  data() {
+    return {
+      images,
+    };
+  },
+  methods: {
+    thumbUrl(filename) {
+      return require(`../images/thumbnails/${filename}`);
+    },
+  },
+};
+</script>
+
+<style>
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+    grid-gap: 1rem;
+    max-width: 80rem;
+    margin: 5rem auto;
+    padding: 0 5rem;
+  }
+  .gallery-panel img {
+    width: 100%;
+    height: 22vw;
+    object-fit: cover;
+    border-radius: 0.75rem;
+  }
+</style>
